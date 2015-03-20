b7bec77af8b98f96f5671bb0da4d7ffb64fdb88d
Luke Campbell <luke.s.campbell@gmail.com>
2015-03-20 14:31:51 -0400

    Adds harvesting of datasets and DatasetModel

--- habs_portal/proxy.py
+++ habs_portal/proxy.py
@@ -9,10 +9,10 @@ from habs_portal import habs_portal as api
 from flask import current_app as app
 from flask import jsonify, request
 import requests
+import json
 
 @api.route('/api/test', methods=['GET'])
 def get_tests():
-
     items = [ {"message":"Hi this is %s" % i} for i in xrange(10) ]
     return jsonify(messages=items)
 
@@ -30,3 +30,31 @@ def get_plot():
     plot_service += '/plotting/getplot/'
     response = requests.get(plot_service, params=request.args)
     return response.content, 200, dict(response.headers)
+
+def _get_datasets():
+    '''
+    Opens the JSON file for the datasets and returns the contents.
+    '''
+    with open('habs_portal/static/json/dataset.json', 'r') as f:
+        records = json.loads(f.read())
+    return records
+
+@api.route('/api/dataset', methods=['GET'])
+def get_datasets():
+    '''
+    Example response:
+    {
+        "datasets": [
+            {
+                "category_id" : "ysi_turbidity",
+                "station_id" : "tollsps",
+                "variables": [
+                    "turbidity"
+                ]
+            }
+        ],
+        "length" : 1
+    }
+    '''
+    return jsonify(**_get_datasets())
+
